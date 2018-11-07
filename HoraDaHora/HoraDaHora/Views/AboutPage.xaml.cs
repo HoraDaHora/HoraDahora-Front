6907ff5e2b56343c25ca0e3a99952a6a377260cc
Cleber Castro <cjj.castro3@gmail.com>
2018-11-07 08:27:55 -0200

    Create Xamarin project

---
+++ HoraDaHora/HoraDaHora/Views/AboutPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HoraDaHora.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Login()
+        {
+
+        }
+    }
+}
---
+++ HoraDaHora/HoraDaHora/Views/LoginPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HoraDaHora.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Login(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new Views.MainPage();
+        }
+    }
+}
